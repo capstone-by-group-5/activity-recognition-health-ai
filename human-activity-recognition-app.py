a401a08137b2d34d5dbcf134e9f07c398ece6872
Dheeraj Kumar <54371995+kdheeraj1502@users.noreply.github.com>
2025-04-16 10:38:22 +0530

    HAR Implementation

--- human-activity-recognition-app.py
+++ human-activity-recognition-app.py
@@ -1357,7 +1357,7 @@ def main():
                                     ax.set_title(f"{model_name} Confusion Matrix", fontsize=5)
                                     st.pyplot(fig)
 
-                                    # Show classification report
+                                    #  classification report
                                     st.write("Classification Report:")
                                     report = classification_report(
                                         st.session_state.y_test,
